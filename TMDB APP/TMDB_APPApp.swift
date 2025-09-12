d43fe96282d31a6d9c8cc22282eb6f7ac980316b
Aman Sikarwar <amansik.1910@gmail.com>
2025-09-13 02:14:06 +0530

    Initial Commit

---
+++ TMDB APP/TMDB_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_APPApp.swift
+//  TMDB APP
+//
+//  Created by Aman Sikarwar on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
